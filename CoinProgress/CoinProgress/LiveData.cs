2af9f165f7353555f4db4e494e99044064167410
Meric AYDIN <101329548+HMericAydin@users.noreply.github.com>
2023-10-23 21:43:21 +0300

    Add files via upload

---
+++ CoinProgress/CoinProgress/LiveData.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinProgress
+{
+    public class LiveData
+    {
+        private readonly HttpClient _client;
+
+        public LiveData()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<float> CoinSelectionAsync(string crypto)
+        {
+            float price = 0;
+
+            try
+            {
+                var response = await _client.GetAsync("https://api.binance.com/api/v1/ticker/price?symbol=" + crypto);
+                var content = await response.Content.ReadAsStringAsync();
+                dynamic data = JsonConvert.DeserializeObject(content);
+                price = data.price;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching {crypto} price: {ex.Message}");
+            }
+
+            return price;
+        }
+
+        public async Task CryptoFollowAsync(string crypto)
+        {
+            float price = await CoinSelectionAsync(crypto);
+            Console.WriteLine($"{crypto} = {price}");
+        }
+    }
+}
---
+++ CoinProgress/CoinProgress/Program.cs
@@ -0,0 +1,37 @@
+﻿using CoinProgress;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string[] CryptoCurrencies = new string[] { "BTCUSDT", "ETHUSDT", "XRPUSDT", "LTCUSDT", "BCHUSDT", "ADAUSDT", "DOTUSDT", "LINKUSDT", "XLMUSDT", "EOSUSDT", "XMRUSDT", "XTZUSDT", "TRXUSDT", "BNBUSDT", "VETUSDT", "NEOUSDT", "DASHUSDT", "ZECUSDT", "IOTAUSDT", "ATOMUSDT", "DOGEUSDT", "ALGOUSDT", "FILUSDT", "XEMUSDT", "WAVESUSDT", "UNIUSDT", "MKRUSDT", "ENJUSDT", "SUSHIUSDT", "SNXUSDT", "YFIUSDT", "COMPUSDT" };
+        LiveData coin = new LiveData();
+        /*Console.WriteLine("Enter cryptocurrency name: ");
+        string crypto = Console.ReadLine();
+        Console.WriteLine("The initial value is = " + await coin.CoinSelectionAsync(crypto));*/
+
+        List<Task> tasks = new List<Task>();
+
+        foreach (string currency in CryptoCurrencies)
+        {
+            tasks.Add(UpdateCryptoPrice(coin, currency));
+        }
+
+        await Task.WhenAll(tasks);
+    }
+
+    static async Task UpdateCryptoPrice(LiveData coin, string currency)
+    {
+        while (true)
+        {
+            await coin.CryptoFollowAsync(currency);
+            await Task.Delay(500); // Wait for 0.5 seconds
+        }
+    }
+}
